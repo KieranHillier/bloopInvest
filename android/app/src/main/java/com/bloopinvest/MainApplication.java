c212cd69bba84756717663acb1c06320cde1d233
Kieran Hillier <kieranhillier@outlook.com>
2018-09-26 21:46:27 -0400

    auth & cloud functions

--- android/app/src/main/java/com/bloopinvest/MainApplication.java
+++ android/app/src/main/java/com/bloopinvest/MainApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.functions.RNFirebaseFunctionsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,7 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
           new RNFirebasePackage(),
-          new RNFirebaseFirestorePackage()
+          new RNFirebaseFirestorePackage(),
+          new RNFirebaseAuthPackage(),
+          new RNFirebaseFunctionsPackage()
       );
     }
 
